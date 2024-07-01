7c7220585f68c6b3c339cdff7318c15bcb25082e
SSJMUNDEN2 <132748138+SSJMUNDEN2@users.noreply.github.com>
2024-07-01 15:03:52 +0200

    Delete FSWD_FS24_PowerDate-main directory

--- FSWD_FS24_PowerDate-main/src/main/java/ch/zhaw/fswd/powerDate/entity/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.zhaw.fswd.powerDate.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
